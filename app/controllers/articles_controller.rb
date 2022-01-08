a29f1631fa4e9fa155665c18e8726f8f0b18f1ea
unknown <aki.jazz1027@gmail.com>
2022-01-08 20:42:09 +0900

    createアクションを実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,11 +8,22 @@ class ArticlesController < ApplicationController
   end
 
   def create
+    if current_user
+      Article.create(article_params)
+    else
+      ## errorっぽいやつを作る
+      render json: error
+    end
   end
 
   def edit
   end
 
   def destroy
+  end
+
+  private
+  def article_params
+    params
   end
 end
